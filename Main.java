df37b98d1b3551bbc5fd46187be4b08fa7c34d7d
Katherine Bayard <katherinebayard@zipcoders-MBP-6.lan>
2019-02-06 21:41:18 -0500

    finished SumOfInput lab

--- Main.java
+++ Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
-    public static void main(String[] args){
-
+    private int number;
+    
+    public void main(){
+        number = getNumber();
+        System.out.println(addNumbers(number));
+        
     }
+    
+    public int getNumber() {
+        Scanner in = new Scanner(System.in);
+            
+        System.out.println("Pick a number.");
+     
+        number = in.nextInt(); 
+        return number;
+    }
+
+    public int addNumbers(int number) 
+    {
+        int sum = 0;
+            for(int i = 0; i <= number; i++) {
+            sum = sum + i;
+           }   
+	return sum;
+    }
+
+    
 }
